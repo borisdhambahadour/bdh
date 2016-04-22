f43e73c5e7d052f8285edaff365f9a0b09ab66ab
borisdhambahadour <boris.dhambahadour@gmail.com>
2016-04-22 15:08:36 +0200

    Ajoutez des fichiers projet.

---
+++ SharePointAddInTEST/Features/Lists/Lists.feature
@@ -0,0 +1,9 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="9f505332-4543-4e5d-9201-bec892fc8c80" description="Fonctionnalité de complément SharePoint" featureId="9f505332-4543-4e5d-9201-bec892fc8c80" imageUrl="" solutionId="00000000-0000-0000-0000-000000000000" title="SharePointAddInTEST Feature1" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="e0ffa79c-fb1d-41be-a228-748b19fd8ada" />
+    <projectItemReference itemId="41ba9b11-ac4b-468c-b8b3-b4d6cdf64876" />
+    <projectItemReference itemId="1d549aa8-9122-409f-93cf-c360e4ed5c32" />
+    <projectItemReference itemId="af1cd761-dfea-4ca9-bb21-785b1cb5e42e" />
+  </projectItems>
+</feature>
---
+++ SharePointAddInTEST/Scripts/App.js
@@ -0,0 +1,80 @@
+﻿'use strict';
+
+ExecuteOrDelayUntilScriptLoaded(initializePage, "sp.js");
+
+function initializePage() {
+    var context = SP.ClientContext.get_current();
+    var user = context.get_web().get_currentUser();
+
+   
+    // Cette fonction prépare, charge, puis exécute une requête SharePoint pour obtenir des informations sur les utilisateurs actuels
+    function getUserName() {
+        context.load(user);
+        context.executeQueryAsync(onGetUserNameSuccess, onGetUserNameFail);
+    }
+
+    // Cette fonction est exécutée si l'appel ci-dessus est réussi
+    // Elle remplace le contenu de l'élément 'message' par le nom de l'utilisateur
+    function onGetUserNameSuccess() {
+        $('#message').text('Hello ' + user.get_title());
+    }
+
+    // Cette fonction est exécutée en cas d'échec de l'appel ci-dessus
+    function onGetUserNameFail(sender, args) {
+        alert('Failed to get user name. Error:' + args.get_message());
+    }
+
+    var TestApp = window.TestApp || {};
+
+    TestApp.Customer = function (_id, _title, _firstName, _lastName, _phoneNumber) {
+
+        var f_getID = function () { return _id; },
+        f__getTitle = function () { return _title; },
+        f_getFirstName = function () { return _firstName; },
+        f_getLastName = function () { return _lastName; },
+        f_getPhoneNumber = function () { return _phoneNumber; };
+
+
+        return {
+            getID: f_getID,
+            getTitle: f__getTitle,
+            getFirstName: f_getFirstName,
+            getLastName: f_getLastName,
+            getPhoneNumber: f_getPhoneNumber
+        };
+    }
+
+    TestApp.AllService = function () {
+        var _allCustomers = ko.observableArray();
+        var f_getCustomer = function () {
+            var web = context.get_web(),
+                   list = web.get_lists().getByTitle('Customer'),
+                   items = list.getItems();
+
+            var customers = context.loadQuery(items);
+
+            context.executeQueryAsync(function () {
+                customers.forEach(function (customer) {
+                    _allCustomers.push(new TestApp.Customer(customer.id, customer.title, customer.firstName, customer.lastName, customer.phoneNumber));
+                });
+            }, function (error) {
+                console.log(error);
+            });
+        };
+
+        return {
+            allCustomers: _allCustomers,
+            // ville : _ville,
+            // villeDescription : _villeDescription,
+            get_Customers: f_getCustomer
+        };
+    }
+    // Ce code s'exécute quand le modèle DOM est prêt. Par ailleurs, il crée un objet de contexte nécessaire à l'utilisation du modèle objet SharePoint
+    $(document).ready(function () {
+        getUserName();
+        var customersService = new TestApp.AllService();
+        ko.applyBindings(customersService, document.getElementById('customerSVC'));
+        customersService.get_Customers();
+    });
+
+}
